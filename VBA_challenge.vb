44940e324db0d599482925ea13bea39e171f7783
Kiran Goswitz <goswitz.kiran@gmail.com>
2021-04-02 12:12:42 -0500

    VBA homework second draft

--- VBA_challenge.vb
+++ VBA_challenge.vb
@@ -3,10 +3,47 @@ Sub TickerSummary()
 Dim ws As Worksheet
 
 For Each ws In Sheets
+    dim FirstPrice as double
+    dim LastPrice as double
+    dim RowCounter as integer
+    dim TotChange as double
+    dim PctChange as double
+
+    FirstPrice=ws.Cells(2,3).value
+    volume=0
     last_row = ws.Cells(Rows.Count, 1).End(xlUp).Row
-    
-    
-    
+    RowCounter=2
+    for i = 2 to last_row
+        volume=volume+ws.Cells(i,7).value
+
+        if ws.Cells(i+1,1).value <> ws.Cells(i,1).value Then
+            LastPrice=ws.Cells(i,6).value
+            TotChange=FirstPrice-LastPrice
+            PctChange=TotChange/FirstPrice
+            ws.Cells(RowCounter,9).Value=ws.Cells(i,1).value
+            ws.Cells(RowCounter,10).Value=TotChange
+            ws.Cells(RowCounter,11).Value=PctChange
+            ws.Cells(RowCounter,12).Value=volume
+            FirstPrice=ws.Cells(i+1,3).Value
+            volume=0
+            RowCounter=RowCounter+1
+        End if
+    Next i
+
+    ws.Cells(1,9).Value="Ticker"
+    ws.Cells(1,10).Value="Yearly Change"
+    ws.Cells(1,11).Value="Percent Change"
+    ws.Cells(1,12).Value="Total Volume"
+
+    for p = 2 to RowCounter
+        if ws.Cells(p,10).value > 0 Then
+            ws.Cells(p,10).Interior.ColorIndex=4
+        elseif ws.Cells(p,10).Value < 0 Then
+            ws.Cells(p,10).Interior.ColorIndex=3
+        else
+            ws.Cells(p,10).Interior.ColorIndex=6
+        end if
+    next p      
 Next ws
 
 
